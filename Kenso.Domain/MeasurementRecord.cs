c07924dcf388157b2b37dc39c72ff23bd6bbd595
Tomasz Czerkas <tomeqc@gmail.com>
2023-10-15 21:33:26 -0400

    added MeasurementRecord

---
+++ Kenso.Domain/MeasurementRecord.cs
@@ -0,0 +1,23 @@
+﻿namespace Kenso.Domain
+{
+    public class MeasurementRecord
+    {
+        public string? ModelName { get; set; }
+        public string? PartName { get; set; }
+        public string PartNumber { get; set; }
+        public string? FeatureName { get; set; }
+        public string CharacteristicName { get; set; }
+        public decimal? Nominal { get; set; }
+        public decimal? Usl { get; set; }
+        public decimal? Lsl { get; set; }
+        public decimal? UslWarn { get; set; }
+        public decimal? LslWarn { get; set; }
+        public string? Unit { get; set; }
+        public DateTime MeasurementDate { get; set; }
+        public decimal? MeasurementValue { get; set; }
+        public decimal? Deviation { get; set; }
+        public string? Device { get; set; }
+        public string? Operator { get; set; }
+    }
+}
+
